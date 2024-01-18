9ced2f3790977547fa790f1033d92bd31893b972
jaroslaw-filipiak <info@j-filipiak.pl>
2024-01-18 17:45:04 +0100

    top bar init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
-import { createApp } from 'vue';
+// import { createApp } from 'vue';
 // import './style.scss';
-import App from './App.vue';
+// import App from './App.vue';
 
-createApp(App).mount('#app');
+// createApp(App).mount('#app');
 
 import './scss/main.scss';
+
+const handleHamburger = () => {
+  const hamburger = document.querySelector('.hamburger');
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('is-active');
+  });
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+  handleHamburger();
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      light: '#F9F2E5',
+      dark: '#323232',
+    },
     extend: {},
   },
   plugins: [],
