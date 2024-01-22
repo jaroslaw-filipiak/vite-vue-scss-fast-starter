0858f2c08bca6552d1e34275be5b7e94c417e193
jaroslaw-filipiak <info@j-filipiak.pl>
2024-01-22 18:04:08 +0100

    changes in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,7 @@
 import { defineConfig } from 'vite';
-// import vue from '@vitejs/plugin-vue';
-import { resolve } from 'path';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    // outDir: 'assets',
-    watch: {},
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        typo: resolve(__dirname, 'typo.html'),
-      },
-      // output: {
-      //   entryFileNames: `js/scripts.js`,
-      //   chunkFileNames: `js/scripts.js`,
-      //   assetFileNames: `[name].[ext]`,
-      // },
-    },
-  },
-  // plugins: [vue()],
+  plugins: [vue()],
 });
