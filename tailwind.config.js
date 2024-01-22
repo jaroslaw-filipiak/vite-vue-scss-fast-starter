a5a85afddb2877c5bdeb0019fa2a2180ee300401
jaroslaw-filipiak <info@j-filipiak.pl>
2024-01-22 13:56:57 +0100

    handle fluid typo

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,30 @@ export default {
       light: '#F9F2E5',
       dark: '#323232',
       dark2: '#9F9F9F',
+      darkMain: '#1E1E1E',
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-fluid-type')({
+      settings: {
+        fontSizeMin: 1.125,
+        fontSizeMax: 1.21,
+        ratioMin: 1.125,
+        ratioMax: 1.2,
+        screenMin: 20,
+        screenMax: 96,
+        unit: 'rem',
+        prefix: 'fluid-',
+      },
+      values: {
+        // ...
+        base: 0,
+        // ...
+      },
+    }),
+  ],
+  variants: {
+    fluidType: ['responsive'],
+  },
 };
