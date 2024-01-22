c7bef287c257c17f9b3a13e42ab1c23bea8ac2ec
jaroslaw-filipiak <info@j-filipiak.pl>
2024-01-22 18:07:37 +0100

    rollupOptions

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        typo: resolve(__dirname, 'typo.html'),
+      },
+      output: {
+        entryFileNames: `js/scripts.js`,
+        chunkFileNames: `js/scripts.js`,
+        assetFileNames: `[name].[ext]`,
+      },
+    },
+  },
   plugins: [vue()],
 });
