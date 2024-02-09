5cb05839f220c3860c911305e282c2f4557f9f7a
jaroslaw-filipiak <info@j-filipiak.pl>
2024-02-09 09:44:12 +0100

    add about page to build process

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         websites: resolve(__dirname, 'websites.html'),
         contact: resolve(__dirname, 'kontakt.html'),
+        about: resolve(__dirname, 'about.html'),
       },
       output: {
         entryFileNames: `js/[name].js`,
